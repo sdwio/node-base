03b411ebe48a2887b314c0d33aeec32e50da7216
SDW <sdw@hg-236-swerner.speedportneo090128250022>
2017-11-16 19:30:47 +0100

    hello world and hello websocket

---
+++ hello-websocket/index.js
@@ -0,0 +1,28 @@
+// content of index.js
+
+const WebSocket = require('ws');
+
+const port = 8080
+
+const server = new WebSocket.Server({ port });
+
+console.log('WebSocket server started on port %i', port)
+
+server.on('connection', function connection(socket) {
+  console.log('connection established')
+  socket.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    socket.send(`received: ${message}`);
+  });
+  socket.send('hello websocket');
+});
+
+console.log('listening for incoming connections')
+
+insideTheConsoleTry = () => {
+    // start the server first of course
+    socket = new WebSocket('ws://localhost:8080');
+    socket.send('well hello there');
+    socket.onmessage = message => console.log(message);
+    socket.send('hello again');
+}
